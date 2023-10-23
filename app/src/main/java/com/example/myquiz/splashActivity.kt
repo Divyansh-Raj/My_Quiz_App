aab0afc4267ae773088c201bd166caa91f8aaa70
Divyansh-Raj <divyanshgupta1406@gmail.com>
2023-10-24 03:15:13 +0530

    added splash screen

---
+++ app/src/main/java/com/example/myquiz/splashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myquiz
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class splashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_quiz_app)
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
